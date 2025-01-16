bb02fed508b35eb060b6cf294008e70903ca3c06
Rakesh RJ <rakeshjakkula139@gmail.com>
2025-01-16 22:44:59 +0530

    myPortfolio Deployment

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
     <section className='py-20 align-element bg-slate-400' id="skills"   >
 
         <div className=' pb-5'>
-                  <h2 className='text-5xl font-medium text-transparent bg-clip-text  bg-gradient-to-r
+                  <h2 className='text-4xl font-medium text-transparent bg-clip-text  bg-gradient-to-r
                    from-rose-600 to-cyan-600 text-center tracking-wider capitalize'>
                        Skills & Expertise
                   </h2>
